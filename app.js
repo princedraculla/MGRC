2ce1199ceb21f325890e283879b26e927403aad2
princedraculla <amirtorkashvand56@gmail.com>
2024-03-16 14:26:09 +0330

    feat(auth): register API

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import db from "./config/database.js"
+import router from "./Routes/userRoutes.js"
 dotenv.config()
-
 const app = express()
+app.use(express.json())
+
+app.use(router)
+
 
 
 const PORT = process.env.SERVER_PORT
