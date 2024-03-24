c94b6bafe993a10a3d4d4c99b132bfe793fa0e65
princedraculla <amirtorkashvand56@gmail.com>
2024-03-25 02:14:29 +0330

    feat(aut): register and login

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import  {register, userList}  from "../controller/user.controller.js"
+import  {login, register, userList}  from "../controller/user.controller.js"
 const router = express.Router()
 
 router.post('/api/v1/register', register)
+router.post('/api/v1/login', login)
 router.get('/api/test/list', userList)
 
 
