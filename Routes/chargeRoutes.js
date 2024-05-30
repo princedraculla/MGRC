bc64fd8a054a44a942a6b68a075fa46c0359a41c
princedraculla <amirtorkashvand56@gmail.com>
2024-05-31 00:31:17 +0330

    feat(api): save file for users using multer

--- Routes/chargeRoutes.js
+++ Routes/chargeRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import verifyToken from "../middleware/authMiddleware.js";
-import { userCharge } from "../controller/charge.controller.js";
+import { userCharge, userUpload } from "../controller/charge.controller.js";
 import upload from "../middleware/fileHandler.js";
 const router = express.Router()
 
 
 router.post('/api/v1/charge', verifyToken, userCharge)
-router.post('/api/v1/upload', upload.single('file'), )
+router.post('/api/v1/upload', verifyToken ,upload.single('file'), userUpload)
 
 export  {router}
--- controller/charge.controller.js
+++ controller/charge.controller.js
@@ -1,6 +1,7 @@
 import date from "date-and-time";
 import userSchema from "../model/userModel.js";
 
+
 const userCharge = async (req, res) => {
   const { reciept } = req.body;
   const id = req.userId;
@@ -24,4 +25,20 @@ const userCharge = async (req, res) => {
   }
 };
 
-export { userCharge };
+const userUpload = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const userCanUpload = await userSchema.findById({ _id: userId });
+    if (!userCanUpload.reciept.value > 1000) {
+      return res.status(403).json({
+        msg: "you dont have enough money for this opration!... first charge your account",
+      });
+    } else {
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export { userCharge, userUpload };
